86e1f69abd11476bdb0bea6de1be89b81a6db54f
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-07-20 16:42:56 +0300

    add the empty date validation function

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
+
 maleNames= ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
 femaleNames= ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 userGender = ['M', 'F'];
+maleNames= ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+femaleNames= ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
-alert(userGender[0])
+//==========================================================================================
+//This function validates that the birthday field is not empty
+//==========================================================================================
+
+function validateBirthday(){
+  //var inputValBirthday = document.getElementById("birthDay").value
+  if (document.getElementById("birthDay").value === ''){
+    alert('Please key in your date of birth');
+    document.getElementById("birthDay").value.focus();
+    return false;
+  }
+  else{alert('give me a minute')}
+  }
+
+  //===========================================================================================
+  // This function validates that  the date entered is valid (DDMMYY)
+  //===========================================================================================
+
+  
+  
 
 
 
 
-
-function evenNumCalc()
-{
-var num1 =parseInt(prompt('key in a number'));
- if (num1%2===0){
- alert('that is an even number') ;
- }
- else {
- alert('that is an odd number');
- }
-}
-
-evenNumCalc()
-
-birthDay = parseInt(prompt('date of birth'));
-gender = prompt('gender');
-userGender = ['M', 'F'];
+//birthDay = parseInt(prompt('date of birth'));
+//gender = prompt('gender');
+//userGender = ['M', 'F'];
 //dayOfWk = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-
-maleNames= ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-femaleNames= ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
 
 
@@ -36,34 +41,22 @@ function fetchDay(){
 if (birthDay =1  && userGender[0] === gender){
 alert('you were born on a Sunday and your name is ' + " " + maleNames[0]);
 }
-
 else if (birthDay =2 && userGender[0] === gender){
 alert('you were born on a Monday and your name is ' + " " + maleNames[1]);
 }
 else{
 alert('try again later')
 }
-
 }
 
-fetchDay()
+//fetchDay()
 
-function getInputValue(){
-  // Selecting the input element and get its value 
-  var inputVal = document.getElementById("myInput").value;
-  //var inputValues = document.getElementById("fieldset").querySelectorAll('birthDay', 'gender'); 
-  // Displaying the value
-  alert(inputVal);
- // alert(inputValues);
-}
 var inputValues = []
 function getFieldSet(){
   // Selecting the input element and get its value 
-  //var inputValues = []
   var inputVal = document.getElementById("birthDay").value;
- //var inputValues = document.getElementById("fieldset").querySelectorAll('birthDay', 'gender'); 
-  // Displaying the value
-  //alert(inputVal);
-  inputValues.push(inputVal + 'test')
+  //var inputVal1= document.querySelector(FormData)
+  inputValues.push(inputVal + 'test') //update selected value on a list
 alert(inputValues)
+
 }
